65e373aa960bb0268b38172f91af08ee96f95cd2
youness <adxus405@gmail.com>
2024-05-01 16:24:34 +0100

    styling

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,7 +3,7 @@ import Logo from '../logo/logo'
 
 export default function Header() {
     return (
-        <header className="border-b-2 md:py-4 py-2 md:px-4 px-2">
+        <header className="border-b-2 p-4">
             <Logo />
         </header>
     )
--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -2,6 +2,6 @@ import Link from "next/link";
 
 export default function Logo() {
     return (
-        <Link href={"/"} className="md:text-3xl text-2xl font-semibold tracking-wider flex-1">Tasky</Link>
+        <Link href={"/"} className="text-3xl font-semibold tracking-wider flex-1">Tasky</Link>
     )
 }
