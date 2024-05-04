397695060373a3ae52304b3a0bc6b216dd3c5065
youness <adxus405@gmail.com>
2024-05-04 17:00:55 +0100

    styling

--- components/auth/FormWrapper.tsx
+++ components/auth/FormWrapper.tsx
@@ -10,14 +10,14 @@ interface FormWrapperProps {
 
 export default function FormWrapper({ children, title, backhref, backlabel }: FormWrapperProps) {
     return (
-        <div className='grow md:max-w-xs sm:max-w-sm space-y-2 border rounded-xl shadow-md bg-white max-w-[76%] px-4 py-6 h-fit overflow-hidden'>
+        <div className='grow md:max-w-xs sm:max-w-sm space-y-2 border rounded-xl shadow-md bg-white max-w-[97%] px-4 py-6 h-fit overflow-hidden'>
             <header className="text-center text-3xl mb-4 font-medium">
                 {title}
             </header>
             <div>
                 {children}
             </div>
-            <footer className="text-center">
+            <footer className="text-center ">
                 <hr className="border-1 mt-5" />
                 <Button variant={"link"}>
                     <Link href={backhref} className="mt-3">{backlabel}</Link>
